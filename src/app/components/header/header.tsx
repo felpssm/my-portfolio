ec67417ebd6a6c5853a3a9d95ca4b4626a2af88e
Felipe Ferreira <feelpss.martinssf@gmail.com>
2024-09-19 08:18:15 -0300

    feat: second commit

---
+++ src/app/components/header/header.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+import "./header.scss"
+
+export function Header() {
+    return (
+        <div className="header">
+            <div>
+                <h1>Hi, i'm Felipe!</h1>
+                <h2>Software Enginner</h2>
+            </div>
+            <Image
+                src="/me.jpg"
+                alt="Next.js logo"
+                width={325}
+                height={277}
+                priority
+            />
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,36 @@
 import Image from "next/image";
+import { Header } from './components/header/header'
 
 export default function Home() {
   return (
 
-      <main>
-        <Image
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        
-      </main>
+    <main>
+      <Header/>
+      <div className="Experience">
+        <h3>Experience</h3>
+        <p>Texto for experience</p>
+        <div className="experience-time">
+
+        </div>
+        <div className="infos">
+          <h3>Languages</h3>
+          <div className="language-infos">
+            <span>🇺🇸 EN - Middleman</span>
+            <span>🇧🇷 PT-BR - Native Speaker</span>
+          </div>
+          <h3>Education</h3>
+          <div className="educational-info">
+          <span>🎓</span>
+          <span> Systems analysis and development - Universidade Católica de Pelotas </span>
+          </div>
+        </div>
+        <div className="buttons">
+          <div className="social">
+
+          </div>
+          <button>Contact me</button>
+        </div>
+      </div>
+    </main>
   )
 }
