53e9336f9472742dd391c753dbc4798cfe2d50a3
Felipe Ferreira <feelpss.martinssf@gmail.com>
2024-09-19 17:18:27 -0300

    feat: modified experience and skills

--- src/app/components/experience/experience.tsx
+++ src/app/components/experience/experience.tsx
@@ -10,10 +10,10 @@ export function Experience(){
           <SectionTitle text="Experience"/>
           <p>1 year working as a Python Developer and automation analist, in companys such as IT4US.</p>
           <div className="experience-time">
-            <Skill image="/react.png" measure={2} years="1 years"/>
-            <Skill image="/ts.png" measure={3} years="1 years"/>
-            <Skill image="/js.png" measure={3} years="1 years"/>
-            <Skill image="/java.png" measure={1} years="1 year"/>
+            <Skill image="/react.png" measure={2} years="1 year"/>
+            <Skill image="/ts.png" measure={3} years="1 year"/>
+            <Skill image="/js.png" measure={3} years="1 year"/>
+            <Skill image="/java.png" measure={1} years="06 months"/>
           </div>
         </div>
     )
